a27ea37a7ae3d03fea6d7252a49201baf7d496ba
Amdounislim <amdouni.slim@gmail.com>
2021-08-11 12:38:34 +0100

    first

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+function Header() {
+
+    let name = "Slim Amdouni"
+
+    return(
+        <header>
+            <h1>Hello</h1>
+            <p>I'm {name}</p>
+        </header>
+    );
+    
+}
+
+export default Header
---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Main = () => {
+
+    
+
+
+    return (
+        <div>
+            <h2>Welcome to Main page</h2>
+        </div>
+    )
+}
+
+export default Main
+
+
